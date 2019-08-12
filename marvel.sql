565f95659412bf6063db9e6ed86993211a4daffa
Joe Cooper <josephcooper3@gmail.com>
2019-08-12 16:50:44 +0100

    completed extension

--- marvel.sql
+++ marvel.sql
@@ -53,40 +53,48 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 -- ## Questions
 --
 -- 1.  Return ALL the data in the 'movies' table.
--- SELECT * FROM movies;
+SELECT * FROM movies;
 
 -- 2.  Return ONLY the name column from the 'people' table
--- SELECT name FROM people;
+SELECT name FROM people;
 
 -- 3.  Oops! Someone spelled Krusty The Clown's name wrong! Change it to reflect the proper spelling (Crusty should be Krusty).
 UPDATE people SET name = 'Krusty the Clown' WHERE id = 13;
--- SELECT * FROM people;
 
 -- 4.  Return ONLY Homer Simpson's name from the 'people' table.
--- SELECT name FROM people WHERE name = 'Homer Simpson'
+SELECT name FROM people WHERE name = 'Homer Simpson'
 
 -- 5.  The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
 DELETE FROM movies WHERE title = 'Batman Begins';
--- SELECT * FROM movies;
 
 -- 6.  We forgot one of the main characters! Add Bart Simpson to the 'people' table
 INSERT INTO people (name) VALUES ('Bart Simpson');
--- SELECT * FROM people;
 
 -- 7.  Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
 DELETE FROM people WHERE name = 'Eric Cartman';
--- SELECT * FROM people;
 
 -- 8.  The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
 INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '00:00');
--- SELECT * FROM movies;
 
 -- 9.  The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
 -- SELECT show_time FROM movies WHERE title = 'Iron Man 2';
 UPDATE movies SET show_time = '20:45' WHERE title = 'Iron Man 3';
--- SELECT * FROM movies;
 
 --
 -- ## Extension
 --
 -- 1.  Research how to delete multiple entries from your table in a single command.
+DELETE FROM movies;
+-- deletes all lines from the table
+
+DELETE FROM movies WHERE year = 2017;
+-- deletes all matching that year
+
+DELETE FROM movies WHERE title <> 'Black Panther';
+-- deletes all that do not match
+
+DELETE FROM movies WHERE id BETWEEN 1 AND 10;
+-- deletes all within range
+
+DELETE FROM movies WHERE title LIKE 'Iron Man%';
+-- deletes all titles starting with 'Iron Man'
