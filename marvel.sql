8b4706f9dcf16840f1fbfac7224d49a7104ca2f0
Joe Cooper <josephcooper3@gmail.com>
2019-08-12 15:48:18 +0100

    completed MVP

--- marvel.sql
+++ marvel.sql
@@ -74,8 +74,18 @@ INSERT INTO people (name) VALUES ('Bart Simpson');
 -- SELECT * FROM people;
 
 -- 7.  Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
+DELETE FROM people WHERE name = 'Eric Cartman';
+-- SELECT * FROM people;
+
 -- 8.  The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
+INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '00:00');
+-- SELECT * FROM movies;
+
 -- 9.  The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
+-- SELECT show_time FROM movies WHERE title = 'Iron Man 2';
+UPDATE movies SET show_time = '20:45' WHERE title = 'Iron Man 3';
+-- SELECT * FROM movies;
+
 --
 -- ## Extension
 --
